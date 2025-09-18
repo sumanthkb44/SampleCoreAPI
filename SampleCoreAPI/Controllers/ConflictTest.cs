1041c8d7a0217ed4b16e982678e4850c7399dec5
sumanthkb44 <84563853+sumanthkb44@users.noreply.github.com>
2025-09-18 15:12:20 +0530

    Create ConflictTest.cs

---
+++ SampleCoreAPI/Controllers/ConflictTest.cs
@@ -0,0 +1,11 @@
+namespace SampleCoreAPI.Controllers
+{
+    public class ConflictTest
+    {
+        public string GetConflict()
+        {
+            string conflict = "This is a conflict test.";
+            return conflict;
+        }
+    }
+}
